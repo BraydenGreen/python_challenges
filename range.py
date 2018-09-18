5f43718d7fd98ff3bced67836c82e36d1c4bc978
Brayden Green <brayden.p.green@gmail.com>
2018-09-18 15:56:55 -0600

    adding more doc examples

---
+++ range.py
@@ -0,0 +1,14 @@
+# for i in range(10):
+#     print i
+
+# for i in range(0, 1000, 50):
+#     print i
+
+# a = ['Mary', 'had', 'a', 'little', 'lamb']
+# for i in range(len(a)):
+#     print(i, a[i])
+
+print(range(10))
+
+mylist = list(range(5))
+print(mylist)
